5cfc162387f63abb228feb869d0ecf517e1aaad5
Kim-game3 <zweilous387@icloud.com>
2024-09-10 17:44:15 +0900

    操作の実装と衝突時の処理

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,16 +7,17 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float Height;
     [SerializeField] float Speed;
+    [SerializeField] float Stop_Time;
 
     Vector3 Player;
     Vector3 Velocity_player;
-    private Rigidbody R_player;
-    public bool Flag_Controller;
+    public Rigidbody player;
+    public static bool Flag_Controller;
     private bool Is_Ground;
     // Start is called before the first frame update
     void Start()
     {
-        R_player = GetComponent<Rigidbody>();
+        player = GetComponent<Rigidbody>();
         Flag_Controller = true;
         Is_Ground = false;
     }
@@ -24,11 +25,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       if (PlayerHitting.Is_Hit)
+       {
+            Player_Stan();
+       } 
+
        if(Flag_Controller) 
        {
            Player_Move();
        }
-       R_player.velocity = new Vector3(Velocity_player.x , R_player.velocity.y , Velocity_player.z);
     }
 
     void Player_Move()
@@ -38,21 +43,37 @@ public class PlayerController : MonoBehaviour
         Player = new Vector3(x, 0, z);
         Player.Normalize();
         Velocity_player = Player * Speed;
-
         if (Input.GetButtonDown("Jump"))
         {
             Player_Jump();
         }
+        player.velocity = new Vector3(Velocity_player.x, player.velocity.y, Velocity_player.z);
     }
 
     void Player_Jump()
     {
         if (Is_Ground)
         {
-            R_player.AddForce(transform.up * Height, ForceMode.Impulse);
+            player.AddForce(transform.up * Height, ForceMode.Impulse);
+            Debug.Log("jump");
             Is_Ground = false;
         }
         else return;
+    }
+
+    void Player_Stan()
+    {
+        player.velocity = Vector3.zero;
+        player.angularVelocity = Vector3.zero;
+        Flag_Controller = false;
+        Debug.Log("Stop");
+        Invoke("Player_Repair", Stop_Time);
+    }
+
+    void Player_Repair()
+    {
+        Flag_Controller = true;
+        Debug.Log("Move");
     }
 
     private void OnCollisionEnter(Collision other)
@@ -61,5 +82,6 @@ public class PlayerController : MonoBehaviour
         {
             Is_Ground = true;
         }
+
     }
 }
